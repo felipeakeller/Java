c3cc3c29ffd964b58a6cd355bc40e0080c666847
Felipe <felipeakeller@gmail.com>
2015-09-03 23:55:19 -0300

    Curso Java 8 Alura

---
+++ JavaTeste/src/java8/ClasseAnonima.java
@@ -0,0 +1,36 @@
+package java8;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ClasseAnonima {
+
+	List<String> palavras = Arrays.asList("casa código","alura online", "caelum");
+	
+	public void imprime() {
+		Consumer<String> consumidorString = new Consumer<String>() {
+			@Override
+			public void accept(String s) {
+				System.out.println(s);
+			}
+		};
+		palavras.forEach(consumidorString);
+	}
+	
+	public void ordena() {		
+		palavras.sort(new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.compareTo(o2);
+			}
+		});
+	}
+	
+	/**
+	 * palavras.replaceAll(operator);
+	 * palavras.spliterator();
+	 */
+
+}
---
+++ JavaTeste/src/java8/Teste.java
@@ -0,0 +1,11 @@
+package java8;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		ClasseAnonima classeAnonima = new ClasseAnonima();
+		classeAnonima.ordena();
+		classeAnonima.imprime();
+	}
+
+}
